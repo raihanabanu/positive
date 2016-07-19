aedaa1fff7840c67c48bbe967dec8de24b2fd079
raihanabanu <raihanabanu.s@gmail.com>
2016-07-19 10:18:06 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,13 @@
+# positive
+class number{
+public static void main(String args[]){
+int num=integer.parseInt(args[0]);
+if (num>0)
+{
+System.out.println("the given number is even");
+}
+else
+{
+System.out.println("the given number is odd");}
+}
+}
