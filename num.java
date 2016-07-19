c54cfd0b6fc52fa8e85600021e55e07a44860c89
raihanabanu <raihanabanu.s@gmail.com>
2016-07-19 10:23:18 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,15 @@
+# positive
+class num
+{
+public static void main(args[]){
+int num=integer.parseInt(args[0]);
+if(num%2==0)
+{
+System.out.println("The given number is positive");
+}
+else
+{
+System.out.println("the given number is negative");
+}
+}
+}
